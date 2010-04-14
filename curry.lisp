d86ae9a541661570d66a0bc4a396e27db10188ce
Rudy Neeser <rudy@cs.uct.ac.za>
2010-04-14 15:28:28 +0200

    Added a documentation string.

--- curry.lisp
+++ curry.lisp
@@ -7,6 +7,9 @@
 
 (declaim (ftype (function (function &rest t) (function (&rest t))) curry))
 (defun curry (function &rest arguments)
+  "CURRY returns a function that, when called with N arguments, will
+  call CURRY's original FUNCTION argument, followed by CURRY's
+  remaining ARGUMENTS, and then the N arguments."
   (declare (optimize (speed 3)))
   (lambda (&rest args)
     (apply function (append arguments args))))
